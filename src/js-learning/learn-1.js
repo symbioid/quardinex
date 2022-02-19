3aafbf53ab23d7648dc79456ac01f6dc096ec82a
symbioid <symbioid@gmail.com>
2022-02-18 20:16:11 -0600

    Good.  Builds Board properly.  All connections. Build walker next.

--- src/js-learning/learn-1.js
+++ src/js-learning/learn-1.js
@@ -24,14 +24,6 @@ let node = {
 	targets : []
 }
 
-/*let buildNode = (row, col, val) => {
-	let tempNode = { ... node }
-	tempNode.row = row;
-	tempNode.col = col;
-	tempNode.value = val;
-	return tempNode;
-}*/
-
 let buildNode = (row, col, val) => {
 	return {
 	  row : row,
@@ -71,44 +63,31 @@ let displayBoard = (board) => {
 let addTargets = (board) => {
 	for(row=0; row < boardSize; row++) {
 		for (col=0; col < boardSize; col++ ) {
-			// console.log(`Board[${row}][${col}] =  ${board[row][col].value}`);
 			for (i = 0; i < numPolySides; i++) {
 				if(i % 2 == 0) {
-					// console.log(`directionMatrix: ${directionMatrix[i]}`);
 					targetRow = row + (board[row][col].value * directionMatrix[i]);
 					targetCol = col;
 				} else {
-					// console.log(`directionMatrix: ${directionMatrix[i]}`);
 					targetRow = row;
 					targetCol = col + (board[row][col].value * directionMatrix[i]);
 				}
-				// console.log(`TARGET [${i}] ACQUIRED: [${targetRow}][${targetCol}]`);
-
-				//------bounds checking - any target whose (row | col) (< 0 | >boardsize) get set "NULL"
+				//Bounds Check, if not in board, assign null, otherwise, assign new object
+				//based on values calculated above.
                 if ((targetRow < 0 || targetRow>= boardSize) || (targetCol < 0  || targetCol >=  boardSize)) {
 					board[row][col].targets[i] = null;
 					console.log("null ",board[row][col].targets[i],row,col,i)
-					//	console.log(`[${targetRow}][${targetCol}] ASSIGNING TARGET[${i}] = [NULL] // (Out Of Bounds)`)
 				}
 				else {
-					//
-					let poop = {
+					let node = {
 						row: board[targetRow][targetCol].row,
 						col: board[targetRow][targetCol].col,
 						value: board[targetRow][targetCol].value,
 					}
-					board[row][col].targets[i] = poop;
-			//		board[row][col].targets[i] = {value:board[targetRow][targetCol].value}; <-- WORKS-ISH
-			//		console.log(poop)
+					board[row][col].targets[i] = node;
 					console.log("undefined ", board[row][col].targets[i],row,col,i)
-					//board[row][col].targets[i] = Object.assign({},board[targetRow][targetCol]);
-					// console.log(`[${targetRow}][${targetCol}] ASSIGNING TARGET[${i}] = [${targetRow}][${targetCol}]`)
 				}
 			}
-			//console.log(board[row][col]);
-			console.log("-");
 		}
-		console.log("-")
 	}
 }
 showTargets = (node) =>{
@@ -125,5 +104,3 @@ showTargets = (node) =>{
 board = buildBoard();
 displayBoard(board);
 addTargets(board);
-//setTimeout(()=>console.log(board),1000);
-
