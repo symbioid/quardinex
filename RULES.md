b24a7ffa2194240765996712009e46643d42cb68
symbioid <symbioid@gmail.com>
2025-01-22 17:30:14 -0600

    1. fixed numbers on levels. 2. Verified all solvable. 3. Changed order of puzzles based on difficulty.

---
+++ RULES.md
@@ -0,0 +1,42 @@
+Welcome to Quardinex! 
+Your mission is to visit every square on the board.
+
+#----------------------
+[Turquoise Square] = You Are Here (Source)
+[Orange Square] = You Can Go Here (Target)
+[Numbers] = Move Amount
+[PeachPuff3 Squares]  
+#----------------------
+
+You can move [Up, Down, Left, or Right].
+The number of spaces you can move from your position is determined by the number on your [Turquoise Square].
+When you click on an Orange Target Square, that becomes your new position.
+The Orange target is changed to Turquoise.
+The [Turquoise Square you just moved from is now removed from the board presenting a black hole,
+where a Square used to be.
+Keep going until you land on one final square.
+You beat the level.
+
+---------------------
+Select the next level (you can enter any level at any time; there is no limit or "unlock" required)
+
+Some boards may have more than one solution.
+Upcoming Features
+
+Popup "Reload" button when no more moves left.
+Popup "Next Level" button after beating level.
+Still working on the graph solver for this thing;
+it's theoretically so easy, but...
+
+Solve Graph - calculate how many solutions per level, divide by (m,n) (e.g. (rows, columns))
+Gives base difficulty.
+Expand with this knowledge:
+- "Assistance with tips.  
+- If you want a "Hard" assist, you can have the one with least possible exits from here." 
+- If you want "Easy" assist, the one with most possible solutions.  
+
+* Why does my brain want to call this shoaring? 
+--------
+Base difficulty converts to scaling and level "flow"/presentation.
+I picture 3 base sizes (3-5) maybe unlock 6-9 or something.
+
--- src/App.jsx
+++ src/App.jsx
@@ -2,213 +2,210 @@ import React, { Fragment, useState, useEffect } from 'react';
 import './equalize.css';
 import './App.css';
 
+var jsnx = require('jsnetworkx');
+
 function getRandomInt(max) {
   return Math.floor(Math.random() * max);
 }
 
 const levels = [
-  [ // 1 start @ [0][0]
+  [ // 1 start @ [0][1]  diff 1.286
     [2,1,1],
     [1,1,2],
     [1,1,2],
   ],
-  [ // 2 start @ [2][2]
-    [1,1,1,1],
-    [1,1,1,1],
-    [1,2,2,1],
-    [1,1,1,1],
-  ],
-  [ // 3 start @ [2][2]
+  [ // 3 start @ [2][2]   0.50
     [2,1,1,3],
     [2,1,2,1],
     [1,2,1,2],
     [1,2,1,2],
   ],
-  [ // 4 start @ [1][1]
-    [3,1,2,2],
-    [1,2,2,2],
-    [3,1,1,1],
-    [2,1,1,3],
-  ],
-  [ // 5 start @ [0][0]
-    [2,2,2,1],
-    [2,1,1,1],
-    [1,2,2,1],
-    [2,1,1,2],
-  ],
-  [ // 6 start @ [1][2]
-    [2,2,1,3],
-    [1,2,1,1],
-    [3,1,2,1],
-    [1,1,1,2],
-  ],
-  [ // 7 start @ [3][3]
-    [1,1,1,2],
-    [1,1,1,2],
-    [1,1,2,1],
-    [2,1,3,3],
-  ],
-  [ // 8 start @ [0][1]
-    [2,2,3,1],
-    [2,2,1,1],
-    [1,1,1,1],
-    [3,2,2,3],
-  ],
-  [ // 9 start @ [0][0]
-    [3,1,1,3],
-    [3,1,2,1],
-    [3,1,2,1],
-    [2,1,1,3],
-  ],
-  [ // 10 start @ [0][0]
-    [2,1,2,2],
-    [2,2,2,1],
-    [1,1,1,2],
-    [1,2,3,1],
-  ],
-  [ // 11 start @ [0][0]
-    [4,2,4,1,3],
-    [3,2,1,1,1],
-    [1,1,1,1,3],
-    [2,1,1,1,1],
-    [2,3,1,2,1],
-  ],
-  [ // 12 start @ [2][2]
-    [1,1,2,1,1],
-    [1,1,1,1,1],
-    [1,3,2,2,1],
-    [2,1,2,2,1],
-    [2,3,1,2,1],
-  ],
-  [ // 13 start @ [2][2]
-    [3,1,2,2,2],
-    [1,1,2,1,2],
-    [2,1,1,1,1],
-    [1,2,1,1,4],
-    [1,2,2,1,1],
-  ],
-  [ // 14 start @ [2][2]
-    [1,2,1,2,2],
-    [2,1,2,2,1],
-    [2,3,2,3,1],
-    [1,3,1,2,1],
-    [3,1,1,1,3],
-  ],
-  [ // 15 start @ [1][1]
+  [ // 15 start @ [1][1]  0.04
     [3,2,1,1,4],
     [2,3,1,3,1],
     [1,2,2,1,2],
     [1,1,2,1,1],
     [2,1,1,1,2],
   ],
-  [ // 16 start @ [0][0]
-    [3,1,1,3,1],
-    [1,1,1,2,1],
-    [2,2,1,2,1],
-    [1,1,2,1,3],
-    [4,2,1,1,4],
+  [ // 8 start @ [0][1]  0.67
+    [2,2,3,1],
+    [2,2,1,1],
+    [1,1,1,1],
+    [3,2,2,3],
   ],
-  [ // 17 start @ [1][2]
+  [ // 12 start @ [4][0]  0.10
+    [1,1,2,1,1],
+    [1,1,1,1,1],
+    [1,3,2,2,1],
     [2,1,2,2,1],
-    [1,1,3,1,1],
-    [2,2,2,1,2],
-    [1,1,3,1,1],
-    [1,2,2,1,1],
+    [2,3,1,2,1],
   ],
-  [ // 18 start @ [4][0]
-    [1,2,3,3,1],
-    [1,1,1,1,2],
-    [2,2,2,3,2],
-    [1,1,2,2,3],
-    [4,2,4,1,3],
-  ],
-  [ // 19 start @ [2][1]
-    [4,2,1,1,2],
-    [3,1,1,1,3],
-    [2,1,1,3,2],
-    [2,2,1,1,3],
-    [1,1,1,2,1],
-  ],
-  [ // 20 start @ [2][1]
-    [1,1,1,3,3],
-    [2,3,2,3,1],
-    [2,1,1,1,2],
-    [1,2,2,3,1],
-    [2,2,2,2,1],
-  ],
-  [ // 21 start @ [4][4]
+  [ // 21 start @ [4][4]  0.125
     [4,1,1,3,3],
     [3,1,1,1,3],
     [1,1,2,1,1],
     [4,1,1,2,2],
     [3,2,1,2,4],
   ],
-  [ // 22 start @ [0][0]
-    [1,2,2,1,2],
-    [2,3,1,1,1],
-    [1,3,1,2,4],
-    [3,1,2,3,4],
-    [2,1,1,1,4],
+  [ // 17 start @ [4][3]  0.13
+    [2,1,1,2,1],
+    [1,1,3,1,1],
+    [2,2,2,1,2],
+    [1,1,1,1,1],
+    [1,2,2,1,1],
   ],
-  [ // 23 start @ [2][2]
+  [ // 13 start @ [2][2]  0.16
+    [3,1,2,2,2],
+    [1,1,2,1,2],
+    [2,1,1,1,1],
+    [1,2,1,1,4],
+    [1,2,2,1,1],
+  ],
+  [ // 20 start @ [3][1]  0.25
+    [1,1,1,3,3],
+    [2,3,2,3,1],
+    [2,1,1,1,2],
+    [1,2,2,3,1],
+    [2,2,2,2,1],
+  ],
+  [ // 7 start @ [3][3]  1.00
+    [1,1,1,2],
+    [1,1,1,2],
+    [1,1,2,1],
+    [2,1,3,3],
+  ],
+  [ // 2 start @ [2][2]   1.60
+    [1,1,1,1],
+    [1,1,1,1],
+    [1,2,2,1],
+    [1,1,1,1],
+  ],
+  [ // 4 start @ [1][1]   1.60
+    [3,1,1,2],
+    [1,2,2,2],
+    [3,1,1,1],
+    [2,1,1,3],
+  ],
+  [ // 10 start @ [0][0]  1.60
+    [2,1,2,2],
+    [2,2,2,1],
+    [1,1,1,2],
+    [1,2,3,1],
+  ],
+  [ // 11 start @ [0][0]  0.40
+    [4,2,4,1,3],
+    [3,2,1,1,1],
+    [1,1,1,1,3],
+    [2,1,1,1,1],
     [2,3,1,2,1],
-    [1,1,2,3,2],
-    [1,1,2,1,1],
-    [1,2,1,1,3],
-    [1,1,2,1,1],
   ],
-  [ // 24 start @ [0][0]
+  [ // 19 start @ [2][1]  0.42
+    [4,2,1,1,2],
+    [3,1,1,1,3],
+    [2,1,1,3,2],
+    [2,2,1,1,3],
+    [1,1,1,2,1],
+  ],
+  [ // 14 start @ [2][2]  0.45
+    [1,2,1,2,2],
+    [2,1,2,2,1],
+    [2,3,2,3,1],
+    [1,3,1,2,1],
+    [3,1,1,1,3],
+  ],
+  [ // 5 start @ [0][0]   2.00
+    [2,2,2,1],
+    [2,1,1,1],
+    [1,2,2,1],
+    [2,1,1,2],
+  ],
+  [ // 9 start @ [0][0]  2.67
+    [3,1,1,3],
+    [3,1,2,1],
+    [3,1,2,1],
+    [2,1,1,3],
+  ],
+  [ // 24 start @ [0][0]  0.64
     [3,4,3,1,3],
     [1,3,1,3,1],
     [4,1,1,2,2],
     [1,3,1,1,1],
     [2,3,1,4,2],
   ],
-  [ // 25 start @ [2][3]
+  [ // 16 start @ [0][0]  0.78
+    [3,1,1,3,1],
+    [1,1,1,2,1],
+    [2,2,1,2,1],
+    [1,1,2,1,3],
+    [4,2,1,1,4],
+  ],
+  [ // 23 start @ [2][2]  1.087
+    [2,3,1,2,1],
+    [1,1,2,3,2],
+    [1,1,2,1,1],
+    [1,2,1,1,3],
+    [1,1,2,1,1],
+  ],
+  [ // 18 start @ [4][0]  1.39
+    [1,2,3,3,1],
+    [1,1,1,1,2],
+    [2,2,2,3,2],
+    [1,1,2,2,3],
+    [4,2,4,1,3],
+  ],
+  [ // 22 start @ [0][0]  2.27
+    [1,2,2,1,2],
+    [2,3,1,1,1],
+    [1,3,1,2,4],
+    [3,1,2,3,4],
+    [2,1,1,1,4],
+  ],
+  [ // 25 start @ [2][3]  3.57
     [1,1,3,2,1],
     [3,1,2,2,1],
     [1,2,2,2,3],
     [1,2,1,1,3],
     [3,3,4,1,4],
   ],
-];
-
-const startPoint = [
-  [0,0], //1
-  [2,2], //2
-  [2,2], //3
-  [1,1],//4
-  [0,0],//5
-  [1,2],//6
-  [3,3],//7
-  [0,1],//8
-  [0,0],//9
-  [0,0],//10
-  [0,0],//11
-  [2,2],//12
-  [2,2],//13
-  [2,2],//14
-  [1,1],//15
-  [0,0],//16
-  [1,2],//17
-  [4,0],//18
-  [2,1],//19
-  [2,1],//20
-  [4,4],//21
-  [0,0],//22
-  [2,2],//23
-  [0,0],//24
-  [2,3],//25
+  [ // 6 start @ [2][3]  5.33
+    [2,1,1,3],
+    [1,2,2,1],
+    [2,1,1,2],
+    [1,1,3,1],
+  ],
 ]
 
-
-
-
-
+const startPoint = [
+  [0,1], //1
+  [2,2], //3
+  [1,1], //15
+  [0,1], //8
+  [4,0], //12
+  [4,4], //21
+  [4,3], //17
+  [2,2], //13
+  [3,1], //20
+  [3,3], //7
+  [2,2], //2
+  [1,1], //4
+  [0,0], //10
+  [0,0], //11
+  [2,1], //19
+  [2,2], //14
+  [0,0], //5
+  [0,0], //9
+  [0,0], //24
+  [0,0], //16
+  [2,2], //23
+  [4,0], //18
+  [0,0], //22
+  [2,3], //25
+  [2,3], //6
+]
 
 const App = ()=> {
   const [currentLevel, setCurrentLevel] = useState(0);
-  const [selected, setSelected] = useState({ row: startPoint[currentLevel][0], col: startPoint[currentLevel][0] });
+  const [selected, setSelected] = useState({ row: startPoint[currentLevel][0], col: startPoint[currentLevel][1] });
   const [selectedVal, setSelectedVal] = useState(0);
   const [removed, setRemoved] = useState([startPoint[currentLevel][0] + "," + startPoint[currentLevel][1]]);
   const [message, setMessage] = useState("QUARDINEX");
@@ -332,28 +329,25 @@ const App = ()=> {
           <h2>Instructions</h2>
           Welcome to Quardinex! Your mission is to visit every square on the board.<br />
           <hr />
-          [Turquoise Square] : You Are Here<br />
-          [Orange Squares] : You Can Go Here.<br />
-          [Numbers] : Move Amount<br />
-          [Cream Squares] : Use This Info To Help Plan Ahead<br />
+          1. Your goal is to clear the board.<br />
+          2. You are located at the [Turquoise Square].<br />
+          3. The number on the [Turquoise Square] is how many spaces away [Up, Down, Left, Right] that you can move.<br />
+          4. The cream squares cannot be jumped to (they are not valid targets).<br />
+          5. The orange squares are valid targets (using the movement rule from #3).<br />
+          6. Each time you move, the target square you clicked becomes turquoise, and the old turquoise square you were on is removed.<br />
+          7. Continue the above until all you land on the last square, at which point you win; or til you can no longer make a valid move.<br  />
+          8. "Press Reload to Reload the level."<br />
+          9. You may, at any time, press a different level* number.<br />
           <hr />
-          You can move Up, Down, Left, or Right.<br />
-          <hr />
-          The number of spaces you can move from your position is determined by the number on that turquoise square.<br />
-          <hr />
-          When you select an Orange Target, that becomes your new position.<br />
-          The Turquoise Square you just moved from is now removed.<br />
-          <hr />
-          Keep going until you land on one final square.  <br />
-          You beat the level. <br />
-          Select the next level.<br />
-          <hr />
-          Some boards may have more than one solution.<br />
+          Note: Levels are not sorted by any particular difficulty, just by number of squares.<br />
           <hr />
           <strong>Upcoming Features</strong><br />
-          Popup "Reload" button when no more moves left.<br />
           Popup "Next Level" button after beating level.<br />
-          And bigger features that may not ever happen.<br />
+          Backend: Solver that will help build difficulty calculator.<br />
+          Keep track of which possible solutions for each puzzle been completed by player.<br />
+          Hint System (using the above, including the ability to give "ranked" clues (e.g. The "Easy" clue gives you many possible solutions from this step; A "Hard" clue is narrow and fewer possible solutions from this step)).<br />
+          Puzzle Generator (really important for next idea):<br />
+          Dailies, 3 bord sizes and 3 difficulty levels per board size.<br />2
         </div>
 
         <div className="game">
